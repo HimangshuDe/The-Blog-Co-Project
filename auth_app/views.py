9ff31a82729253ac0fe4bece0e2ae434acea6078
HimangshuDe <dehimangshu03@gmail.com>
2024-06-11 07:33:28 +0530

    Initial Commit

---
+++ auth_app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponsePermanentRedirect
+from django.contrib.auth import logout
+from django.urls import reverse
+
+# Create your views here.
+
+def user_login(request):
+    return HttpResponse('Login Page')
+
+def user_register(request):
+    # return HttpResponse('Register Page')
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, 'auth_app/register.html')
+
+def user_logout(request):
+    logout(request)
+    return HttpResponsePermanentRedirect(reverse('index'))
