56db66ffafae0d1807093c1d205bfe5f719cab7a
HimangshuDe <dehimangshu03@gmail.com>
2024-06-11 15:11:04 +0530

    Revamped the UI of Registration Page

---
+++ auth_app/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from auth_app.models import UserModel
+
+# Custom UserCreationForm and UserChangeForm
+
+
+# For adding a new user through admin site
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(required=True, widget=forms.TextInput({'placeholder':'First Name'}))
+    last_name = forms.CharField(required=True, widget=forms.TextInput({'placeholder':'First Name'}))
+    class Meta:
+        model = UserModel
+        fields = ["first_name", "last_name","email"]
+        widgets = {
+            'email':forms.TextInput(attrs={'placeholder':'Email'})
+        }
+        
+    
+    def __init__(self, *args, **kwargs) :
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'placeholder':'Password'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'placeholder':'Confirm Password'})
+
+
+# For updating or changing a user through admin site.
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = UserModel
+        fields = "__all__"
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponsePermanentRedirect
+from django.http import HttpResponse, HttpResponsePermanentRedirect, HttpResponseRedirect
 from django.contrib.auth import logout
 from django.urls import reverse
+
+from auth_app.forms import LoginForm, CustomUserCreationForm
 
 # Create your views here.
 
@@ -10,9 +12,12 @@ def user_login(request):
 
 def user_register(request):
     # return HttpResponse('Register Page')
+    form = CustomUserCreationForm()
     if request.method == 'POST':
-        print(request.POST)
-    return render(request, 'auth_app/register.html')
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            HttpResponseRedirect(reverse('login'))
+    return render(request, 'auth_app/register.html', {'forms':form})
 
 def user_logout(request):
     logout(request)
