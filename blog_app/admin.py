a3b62821cba7c3ca3bfe6a63ef6efe97fd4672e7
HimangshuDe <dehimangshu03@gmail.com>
2024-07-12 14:13:16 +0530

    ℹ️ Updated admin.py

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from blog_app.models import Tag, Post
+from blog_app.models import Post
 
 # Register your models here.
 admin.site.register(Post)
-admin.site.register(Tag)
