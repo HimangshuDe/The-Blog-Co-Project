84dc39bef7f1b7ca2bc461cf140124c4950a4521
HimangshuDe <dehimangshu03@gmail.com>
2024-07-17 14:55:25 +0530

    🚀 Added styling to some elements...
    which were missed out earlier

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -6,10 +6,11 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('register/', views.user_register, name='register'),
     path('account/', views.accounts_view, name='account'),
-    path('verify/email/', views.ForgotPasswordView.as_view(), name='reset-password'),
+    path('verify/email/', views.ForgotPasswordView.as_view(), name='forgot-password'),
     path('verify/wOTP/', views.VerifyOTPView.as_view(), name='otp-verify'),
     path('delete/', views.del_user, name='delete'),
     path('update/', views.update_info_view, name='update'),
     path('logout/', views.user_logout, name='logout'),
     path('update/pass/', views.ChangePasswordView.as_view(), name='update-password'),
+    path('update/pass/reset/', views.PasswordResetView.as_view(), name='reset-password'),
 ]
